5c52546084c1f0f77a6170d915b9b2d0942845d1
arcarver <acarver@byupathway.edu>
2026-09-08 21:54:25 -0700

    It works. put the listen back in

--- server.js
+++ server.js
@@ -44,3 +44,8 @@ app.get('/projects', async (req, res) => {
     const title = 'Service Projects';
     res.render('projects', { title });
 });
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});
