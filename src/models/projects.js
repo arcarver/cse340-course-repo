67cc4ed021c3097e667b1782e591da4289688954
arcarver <acarver@byupathway.edu>
2026-09-15 23:26:23 -0700

    info on projects

--- src/models/projects.js
+++ src/models/projects.js
@@ -2,8 +2,9 @@ import db from './db.js'
 
 const getAllProjects = async() => {
     const query = `
-        SELECT title, description, location, project_date
-      FROM public.project;
+        SELECT organization.name, project.title, project.description, project.location, project.project_date
+      FROM public.project
+      INNER JOIN organization on project.organization_id = organization.organization_id;
     `;
 
     const result = await db.query(query);
