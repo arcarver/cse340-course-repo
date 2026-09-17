a7cad655eca710cd3d2bc0d8cb2dc228287b17d8
arcarver <acarver@byupathway.edu>
2026-09-16 22:02:47 -0700

    project

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,7 +1,14 @@
-app.get('/projects', async (req, res) => {
+// import 
+import { getAllProjects } from "../models/projects";
+
+// define 
+const showProjectsPage = async (req, res) => {
   const project = await getAllProjects();
   // console.log(project);
   
   const title = 'Service Projects';
     res.render('projects', { title, project });
-});
+};
+
+// export 
+export { showProjectsPage };
