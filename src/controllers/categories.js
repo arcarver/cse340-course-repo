66a919ed33c07ab6da70faa0a9ba69f30e01c7e8
arcarver <acarver@byupathway.edu>
2026-09-16 22:07:09 -0700

    errors

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,7 +1,14 @@
-app.get('/categories', async (req, res) => {
+// import 
+import { getAllCategories } from "../models/categories";
+
+// define 
+const showCategoriesPage = async (req, res) => {
   const category = await getAllCategories();
   // console.log(category);
 
     const title = 'Service Project Categories';
     res.render('categories', { title, category });
-});
+};
+
+// export 
+export { showCategoriesPage };
--- src/controllers/errors.js
+++ src/controllers/errors.js
@@ -1,6 +1,13 @@
+// import
+
+// define 
+
 // Test route for 500 errors
-app.get('/test-error', (req, res, next) => {
+const testErrorPage = (req, res, next) => {
     const err = new Error('This is a test error');
     err.status = 500;
     next(err);
-});
+};
+
+// export 
+export { testErrorPage }; 
