47d0ce72bfa8b8c17c10a231766edcd7f79d37eb
arcarver <acarver@byupathway.edu>
2026-09-16 20:18:43 -0700

    added middleware

--- server.js
+++ server.js
@@ -30,6 +30,13 @@ app.set('view engine', 'ejs');
 // Tell Express where to find your templates
 app.set('views', path.join(__dirname, 'src/views'));
 
+// Middleware to log all incoming requests
+app.use((req, res, next) => {
+    if (NODE_ENV === 'development') {
+        console.log(`${req.method} ${req.url}`);
+    }
+    next(); // Pass control to the next middleware or route
+});
 
 /**
  * Routes
@@ -58,7 +65,7 @@ app.get('/projects', async (req, res) => {
 app.get('/categories', async (req, res) => {
   const category = await getAllCategories();
   // console.log(category);
-  
+
     const title = 'Service Project Categories';
     res.render('categories', { title, category });
 });
--- src/models/projects.js
+++ src/models/projects.js
@@ -2,7 +2,7 @@ import db from './db.js'
 
 const getAllProjects = async() => {
     const query = `
-        SELECT organization.name, project.title, project.description, project.location, project.project_date
+        SELECT organization.name, organization.organization_id, project.title, project.description, project.location, project.project_date
       FROM public.project
       INNER JOIN organization on project.organization_id = organization.organization_id;
     `;
@@ -12,4 +12,17 @@ const getAllProjects = async() => {
     return result.rows;
 }
 
-export {getAllProjects}  
+
+
+export { getAllProjects }  
+
+// I will get back to this later 
+// const getUpcomingProjects = async () => {
+//   const number_of_projects = 5;
+//   const query = ` SELECT organization.name, organization.organization_id, project.title, project.description, project.location, project.project_date
+//       FROM public.project
+//       INNER JOIN organization on project.organization_id = organization.organization_id
+//       LIMIT  number_of_projects;
+//     `;
+  
+// }
