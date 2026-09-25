abe1dd7f29d97b7b64401ac4730566c1fbc89572
arcarver <acarver@byupathway.edu>
2026-09-25 14:18:07 -0700

    step 6

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,14 +1,25 @@
 // import 
 import { getAllProjects } from "../models/projects.js";
+import { getUpcomingProjects } from "../models/projects.js";
+import { getProjectDetails } from "../models/projects.js";
 
+const NUMBER_OF_UPCOMING_PROJECTS = 5;
 // define 
 const showProjectsPage = async (req, res) => {
-  const project = await getAllProjects();
+  const project = await getUpcomingProjects(NUMBER_OF_UPCOMING_PROJECTS);
   // console.log(project);
   
-  const title = 'Service Projects';
+  
+  const title = 'Upcoming Service Projects';
     res.render('projects', { title, project });
 };
 
+const showProjectDetailsPage = async (req, res) => {
+  const { project_id } = req.params;
+  const projectDetails = await getProjectDetails(project_id);
+
+  res.render('project.ejs', { project: projectDetails });
+  }
+
 // export 
-export { showProjectsPage };
+export { showProjectsPage, showProjectDetailsPage };
